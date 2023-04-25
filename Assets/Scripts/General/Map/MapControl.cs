855bd5a8d703c0fea95a50a9ccd355675308486e
OscarZapataCebrian <117663964+OscarZapataCebrian@users.noreply.github.com>
2023-04-25 14:12:34 +0200

    Map movement

--- Assets/Scripts/General/Map/MapControl.cs
+++ Assets/Scripts/General/Map/MapControl.cs
@@ -5,16 +5,61 @@ using UnityEngine.SceneManagement;
 
 public class MapControl : MonoBehaviour
 {
+    public GameObject Player;
+    public float Timer, GoalTime;
+    public bool Moving;
+    public MapNode SelNode, TarNode;
+    public List<MapNode> Nodes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Moving = false;
+        SelNode = Nodes[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Moving == false)
+        {
+            Player.transform.position = SelNode.transform.position;
+
+            if (Input.GetAxisRaw("Horizontal") < 0 && SelNode.LeftN != null)
+            {
+                TarNode = SelNode.LeftN;
+                Moving = true;
+            }
+
+            if (Input.GetAxisRaw("Horizontal") > 0 && SelNode.RightN != null)
+            {
+                TarNode = SelNode.RightN;
+                Moving = true;
+            }
+
+            if (Input.GetAxisRaw("Vertical") > 0 && SelNode.TopN != null)
+            {
+                TarNode = SelNode.TopN;
+                Moving = true;
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0 && SelNode.BottomN != null)
+            {
+                TarNode = SelNode.BottomN;
+                Moving = true;
+            }
+        }
+        else
+        {
+            Timer += Time.deltaTime / GoalTime;
+            Player.transform.position = Vector3.Lerp(SelNode.transform.position, TarNode.transform.position, Timer);
+            if(Timer >= 1)
+            {
+                SelNode = TarNode;
+                Moving = false;
+                Timer = 0;
+            }
+        }
     }
 
     public void GoToLevel(string LvID)
--- Assets/Scripts/General/Map/MapNode.cs
+++ Assets/Scripts/General/Map/MapNode.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class MapNode : MonoBehaviour
 {
+    //public Scene MyLevel;
+    public MapNode LeftN, RightN, TopN, BottomN;
+
     // Start is called before the first frame update
     void Start()
     {
